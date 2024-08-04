bdb6ea22bae59de95892e351c88cc0e167b51c68
mingray_hsu <mingray_hsu@perfectcorp.com>
2024-08-05 02:59:31 +0800

    Update Vite configuration to include base URL for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
-      base: '/todo-list-react-vite/',
     }),
   ],
+  base: '/todo-list-react-vite/',
 });
